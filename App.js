4efd6b51e3d1991d58185b0357b0dd33862d72f0
Thoko <thokozile.soko@yahoo.com>
2021-03-30 23:13:53 -0400

    I add a table to the table.js file. So far it's hard coded so need to change that. The title is still not rendering.

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import directoryTitle from "./components/Title"
+import Table from "./components/Table"
 
 function App() {
-  return <directoryTitle />;
+  return (
+    <div>
+      <directoryTitle />
+      <Table />
+    </div>
+  );
 }
 
 export default App;
