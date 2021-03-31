e27e2106896525f7a69fcbecc195207481c851fa
Thoko <thokozile.soko@yahoo.com>
2021-03-30 23:06:37 -0400

    I've added text to the title page. Importing it however is not working. I'll keep going with the setup and see what happnes.

---
+++ components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function directoryTitle (){
+return (
+<div className="container">
+      <div className="jumbotron">
+        <h1>Employee Directory</h1>
+        <p>Welcome to your employee directory</p>
+   </div>
+   </div>     
+);
+}
+export default directoryTitle;
